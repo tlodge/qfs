a0eda65ff2fd97e30c65b3ce9b60b4c4070e4d88
tlodge <tlodge@gmail.com>
2022-11-22 18:19:22 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: './',
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     loader: 'akamai',
     path: '',
   },
 }
-
-module.exports = nextConfig
